cb1887168a4cf86d5a153422991cf7107b599468
Dmitry Terentev <zabey52@gmail.com>
2022-11-27 18:44:07 +0300

    v1

--- 02/02.cpp
+++ 02/02.cpp
@@ -53,14 +53,37 @@ int print_arrayForClassObjects(std::string* arrayForClassObjects, const int quan
         return static_cast<int>(codeCommit::fileOutOpenError);
     }
 
-    fileOut << "Адреса:\t" << std::endl;
+    fileOut << "Всего количество адресов: " << quantityAdresses << std::endl << std::endl;
 
-    for (int i = 0; i < quantityAdresses; i++)
+    fileOut << "Адреса:" << std::endl;
+
+    for (int i = 0; i < quantityAdresses - 1; i++)
     {
-        fileOut << i + 1 << ". " << arrayForClassObjects[i] << std::endl;
+        fileOut << i + 1  << ". " << arrayForClassObjects[i] << std::endl;
     }
     fileOut.close();
 };
+
+//Функция сортировки пузырьком
+void addressSort(std::string* arrayForClassObjects, const int quantityAdresses)
+{
+    bool swapped = false;
+    do {
+        swapped = false;
+        std::string tmp;
+
+        for (int i = quantityAdresses - 1; i > 0; --i) 
+        {
+            if (arrayForClassObjects[i - 1] > arrayForClassObjects[i])
+            {
+                tmp = arrayForClassObjects[i];
+                arrayForClassObjects[i] = arrayForClassObjects[i - 1];
+                arrayForClassObjects[i - 1] = tmp;
+                swapped = true;
+            }
+        }
+    } while (swapped);
+}
 
 //Функция создания текстового динамического массива
 std::string* createTextArr(const int quantityAdresses)
@@ -112,8 +135,9 @@ int main()
     };
     fileIn.close(); //Закрываем файл in.txt
 
-    print_arrayForClassObjects(arrayForClassObjects, quantityAdresses);
-    delete_TextArr(arrayForClassObjects, quantityAdresses);
+    addressSort(arrayForClassObjects, quantityAdresses); //Сортируем адреса
+    print_arrayForClassObjects(arrayForClassObjects, quantityAdresses); //Печатаем адреса в файл
+    delete_TextArr(arrayForClassObjects, quantityAdresses); //Удаляем массив
 
     std::cout << "Результат работы программы смотри в файле out.txt" << std::endl;
 }
